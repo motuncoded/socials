9b53c169bfc503c757ad161b16e6f9c75ccf9b07
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-05-18 21:20:31 +0100

    feat: render the api call for posts"

---
+++ pages/index.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+    //API 
+    const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+    //  const URL = "https://jsonplaceholder.typicode.com/users";
+
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false)
+  // const [users, setUsers] = useState([]);
+
+
+
+
+  const fetchPosts = async() => {
+    setLoading(true)
+    const response = await fetch(BASE_URL);
+    const posts = await response.json();
+    setPosts(posts)
+    setLoading(false)
+
+  }
+
+
+  useEffect(()=>{
+    fetchPosts()
+
+  }, [])
+  if (loading) {
+    return (
+      <div className="loadingContainer">
+        <p className="loadingTitle">Loading</p>
+      </div>)
+  }
+
+  return (
+    <div className="postContainer">
+      <h1>Post Feed</h1> 
+      { posts.map((post: Post) => {
+        return (
+          <div key={post.id} className="postWrapper">
+            <h2 className="postTitle">{post.title}</h2>
+            <p className="text-1xl">{post.body}</p>
+          </div>
+        );
+      })
+        }
+    </div>
+  );
+}
